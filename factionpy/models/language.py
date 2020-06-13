220505bdca02830f2e5a9bb6d5ef1c512da74bad
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2020-06-13 11:40:07 -0700

    updates

--- factionpy/models/language.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-
-from factionpy.backend.database import Base
-
-
-class Language(Base):
-    __tablename__ = "Language"
-    Id = Column(Integer, primary_key=True)
-    Name = Column(String)
